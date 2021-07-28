cc93426088b8249f61a3f258c0228857186c2392
Claudio Junior <Cjrnew@gmail.com>
2021-07-27 23:14:12 -0300

    Functional Interface - Function

---
+++ src/functionalinterfaces/function/FunctionExample.java
@@ -0,0 +1,38 @@
+package functionalinterfaces.function;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionExample {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("Kit", "Kat", "Shake");
+
+        Function<String, Integer> function = String::length;
+
+        List<Integer> newList = map(list, function);
+
+        System.out.println(newList);
+
+
+    }
+
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+
+
+        List<R> newList = new ArrayList<>();
+
+        for (T e : list) {
+
+            newList.add(function.apply(e));
+
+        }
+
+        return newList;
+
+    }
+
+}
